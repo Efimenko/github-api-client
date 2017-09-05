b14e308aad0b0539ac5baf5ce5c96d7a0bee65e2
efimenko bohdan <bogdane@weblium.com>
2017-09-05 11:55:24 +0300

    fix parse date

--- src/util/calc-date.js
+++ src/util/calc-date.js
@@ -3,21 +3,23 @@ export const calcDate = (date) => {
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
   const delta = {}
 
-  delta.days = Math.round((new Date() - update) / 1000 / 60 / 60 / 24)
-  delta.hours = delta.days * 24
-  delta.minutes = delta.hours * 60
-  delta.seconds = delta.seconds * 60
+  delta.days = (new Date() - update) / 1000 / 60 / 60 / 24
+  delta.hours = Math.floor(delta.days * 24)
+  delta.minutes = Math.floor(delta.days * 24 * 60)
+  delta.seconds = Math.floor(delta.days * 24 * 60 * 60)
 
-  if (delta.days > 31) {
-    return `on ${update.getDate()} ${months[update.getMonth()]}
-    ${update.getFullYear() < new Date().getFullYear() ? update.getFullYear() : ''}`
-  } else if (delta.days <= 31 && delta.days >= 1) {
-    return delta.days === 1 ? `${delta.days} day ago` : `${delta.days} days ago`
-  } else if (delta.hours <= 24 && delta.hours >= 1) {
-    return delta.hours === 1 ? `${delta.hours} hour ago` : `${delta.hours} hours ago`
+  console.log(delta);
+
+  if (delta.seconds <= 60 && delta.seconds >= 1) {
+    return delta.seconds === 1 ? `${delta.seconds} second ago` : `${delta.seconds} seconds ago`
   } else if (delta.minutes <= 60 && delta.minutes >= 1) {
     return delta.minutes === 1 ? `${delta.minutes} minute ago` : `${delta.minutes} minutes ago`
-  } else if (delta.seconds <= 60 && delta.seconds >= 1) {
-    return delta.seconds === 1 ? `${delta.seconds} second ago` : `${delta.seconds} seconds ago`
+  } else if (delta.hours <= 24 && delta.hours >= 1) {
+    return delta.hours === 1 ? `${delta.hours} hour ago` : `${delta.hours} hours ago`
+  } else if (delta.days <= 31 && delta.days >= 1) {
+    return Math.round(delta.days) === 1 ? `${Math.round(delta.days)} day ago` : `${Math.round(delta.days)} days ago`
+  } else if (delta.days > 31) {
+    return `on ${update.getDate()} ${months[update.getMonth()]}
+    ${update.getFullYear() < new Date().getFullYear() ? update.getFullYear() : ''}`
   }
 }
